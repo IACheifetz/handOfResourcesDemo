ef42ffd2ee09fef2c4ecd8a5936f12cd424253fe
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-07-07 11:31:39 -0700

    set up third table and initial tests and wrote out blank controller and model

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS jobs;
 DROP TABLE IF EXISTS swords;
+DROP TABLE IF EXISTS helmets;
 
 CREATE TABLE jobs (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -15,4 +16,11 @@ CREATE TABLE swords (
     sword_name TEXT NOT NULL,
     sword_period TEXT NOT NULL,
     metal_type TEXT NOT NULL
+);
+
+CREATE TABLE helmets (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    helmet_name TEXT NOT NULL,
+    helmet_region TEXT NOT NULL,
+    era TEXT NOT NULL
 );
